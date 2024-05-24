e70cebc4a3403deab5b00723581aca8074a5dcf5
Eduardo Capdeville <capdevilleeduardo@gmail.com>
2024-05-24 10:34:32 -0300

    :recycle: Refactor the function to create the table

--- script.js
+++ script.js
@@ -1,46 +1,90 @@
-function convert(jsonData) {
-
-    let resultado = document.getElementById("resultado");
-    resultado.innerHTML = '';
-
-    let table = document.createElement("table");
-
-    let cols = Object.keys(jsonData[0]);
-    cols[3] = "Descrição";
-
-    let tr = document.createElement("tr");
-
-    cols.forEach((item) => {
-        let th = document.createElement("th");
-        th.innerText = item;
-        tr.appendChild(th);
+let dados = [
+    {
+      "Localizador": 28,
+      "Data": "2024-03-29",
+      "Valor": -32.55,
+      "Tipo": "Compra no débito",
+      "Descricao": "Pag*53486958fernando",
+      "Categoria": "Padaria"
+    },
+    {
+      "Localizador": 29,
+      "Data": "2024-03-29",
+      "Valor": -7.81,
+      "Tipo": "Compra no débito",
+      "Descricao": "Pag*53486958fernando",
+      "Categoria": "Padaria"
+    },
+    {
+      "Localizador": 27,
+      "Data": "2024-03-27",
+      "Valor": -70,
+      "Tipo": "Transferência enviada pelo Pix",
+      "Descricao": "Eduardo Capdeville De São Thiago Sardenberg Da Rocha",
+      "Categoria": ""
+    }
+  ];
+  
+  function convert(dados) { // declarando a função criarTabela e passando o array de objeto "dados" como parâmetro
+    let table = document.createElement("table"); // declarando a variável table como sendo a tabela criada no HTML
+  
+    let thead = document.createElement("thead"); // declarando a função thead como sendo o thead no HTML
+    let headerRow = document.createElement("tr"); // declarando a função headerRow como sendo o tr no HTML, que aqui vai ser a linha de cabeçalho
+  
+    let headers = ["Localizador", "Data", "Valor", "Tipo", "Descricao", "Categoria"]; // criando o array headers com as chaves do array de objetos para usar como os headers da tabela
+    headers.forEach(headerText => { // usando o método forEach para dizer que para cada dado (headerText) do array headers vai fazer tal coisa
+        let th = document.createElement("th");// criando a variavel th como sendo o elemento th da tabela HTML
+      th.innerText = headerText; // usando a propriedade innerText no th, passando oque vai estar escrito em cada th, que no caso são os dados do array
+        headerRow.appendChild(th);// appendendo o th no headerRow que é o tr. Na prática vai ser a primeira linha da tabela que vai ser o cabeçalho da tabela
     });
-    table.append(tr)
-
-    jsonData.forEach((item) => {
-        let tr = document.createElement("tr");
-
-        let vals = Object.values(item);
-
-        let BRL = new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-        });
-
-        vals[2] = BRL.format(vals[2]);
-
-        vals.forEach((elem) => {
-            let td = document.createElement("td");
-            td.innerText = elem;
-            tr.appendChild(td);
-        });
-        table.appendChild(tr);
+  
+    thead.appendChild(headerRow); // appendendo a linha de cabeçalho ao thead
+    table.appendChild(thead); // appendendo o thead populado na tabela
+  
+    let tbody = document.createElement("tbody"); // declarando a variavel tbody como sendo o elemento tbody da tabela
+  
+    dados.forEach(dado => { // usando o método forEach no objeto dados, para dizer que para cada dado do array de objetos vai acontecer tal coisa
+        let tr = document.createElement("tr");// declarando a variavel tr como sendo o elemento tr do HTML
+        let tdLocalizador = document.createElement("td");// declarando a variável tdLocalizador como sendo um elemento td do HTML
+        tdLocalizador.innerText = dado.Localizador;// usando a propriedade innerText, passando que o texto do elemento tdLocalizador vai ser o valor da chave Localizador
+        tr.appendChild(tdLocalizador);// appendendo o tdLocalizador à linha tr 
+  
+        let tdData = document.createElement("td"); // declarando a variável tdData como sendo um elemento td do HTML
+        tdData.innerText = dado.Data;// usando a propriedade innerText, passando que o texto do elemento tdData vai ser o valor da chave data
+        tr.appendChild(tdData);// appendendo o tdData à linha tr 
+  
+        let tdValor = document.createElement("td"); // declarando a variável tdValor como sendo um elemento td do HTML
+        tdValor.innerText = dado.Valor;// usando a propriedade innerText, passando que o texto do elemento tdValor vai ser o valor da chave Valor
+        tr.appendChild(tdValor);// appendendo o tdValor à linha tr 
+  
+        let tdTipo = document.createElement("td");// declarando a variável tdTipo como sendo um elemento td do HTML
+        tdTipo.innerText = dado.Tipo;// usando a propriedade innerText, passando que o texto do elemento tdTipo vai ser o valor da chave Tipo
+        tr.appendChild(tdTipo);// appendendo o tdTipo à linha tr 
+  
+        let tdDescricao = document.createElement("td");// declarando a variável tdDescricao como sendo um elemento td do HTML
+        tdDescricao.innerText = dado.Descricao;// usando a propriedade innerText, passando que o texto do elemento tdTipo vai ser o valor da chave Tipo
+        tr.appendChild(tdDescricao);// appendendo o tdDescricao à linha tr 
+  
+        let tdCategoria = document.createElement("td");// declarando a variável tdCategoria como sendo um elemento td do HTML
+        if (dado.Categoria === "") {
+          tdCategoria.innerHTML = "<input type='text' id='nova_categoria' name='nova_categoria'><br><br><button id='atualizar-categoria-btn' onclick='atualizarCategorias()'>Atualizar Categoria</button><br><br>";
+        } else {
+            tdCategoria.innerText = dado.Categoria;// usando a propriedade innerText, passando que o texto do elemento tdCategoria vai ser o valor da chave Categoria
+        }
+        tr.appendChild(tdCategoria); // appendendo o tdCategoria à linha tr
+  
+        tbody.appendChild(tr); // appendendo a linha tr populada com todos as celulas no tbody
     });
-    resultado.appendChild(table)
-}
+  
+    table.appendChild(tbody);// appendendo o tbody populado com a linha tr, na tabela
+    
+    let tabelaContainer = document.getElementById("resultado"); // declarando a variavel tabela container que vai ser o elemento que tem o id resultado no HTML
+    tabelaContainer.innerHTML = "";
+    tabelaContainer.appendChild(table);
+  }
 
 async function getTransactionsByYearAndMonth(tipo, mes, ano) {
-    let itgo = await fetch('http://localhost:8000/transactions'.concat('/', tipo, '/', ano, '/', mes), {
+    let connect1 = await fetch('http://localhost:8000/transactions'.concat('/', tipo, '/', ano, '/', mes), {
         method: "GET"
     }).then((response) => {
         if (response.ok) {
@@ -51,7 +95,7 @@ async function getTransactionsByYearAndMonth(tipo, mes, ano) {
         }
     })
 
-    return itgo;
+    return connect1;
 
 }
 
@@ -93,7 +137,7 @@ async function atualizarCategorias(){
 }
 
 async function atualizeCategories(transacao_id, categoria) {
-    let burn = await fetch('http://localhost:8000/transactions'.concat('/', transacao_id, '/', categoria), {
+    let connect2 = await fetch('http://localhost:8000/transactions'.concat('/', transacao_id, '/', categoria), {
         method: "PUT"
     }).then((response) => {
         if (response.ok) {
@@ -104,5 +148,5 @@ async function atualizeCategories(transacao_id, categoria) {
         }
     })
 
-    return burn;
+    return connect2;
 }
