a538e1a5f210a6bf9eb176e6d14bb386d53df955
Eduardo Capdeville <capdevilleeduardo@gmail.com>
2024-05-27 15:48:51 -0300

    :sparkles: Add new edit feature to the transactions

--- script.js
+++ script.js
@@ -35,21 +35,50 @@ function convert(dados) { // declarando a função criarTabela e passando o arra
         tdDescricao.innerText = dado.Descricao;// usando a propriedade innerText, passando que o texto do elemento tdTipo vai ser o valor da chave Tipo
         tr.appendChild(tdDescricao);// appendendo o tdDescricao à linha tr 
 
-        let tdCategoria = document.createElement("td");// declarando a variável tdCategoria como sendo um elemento td do HTML
+        let tdCategoria = document.createElement("td"); // Declarando a variável tdCategoria como sendo um elemento td do HTML
 
-        let caixaDeTexto = document.createElement("input")
-        caixaDeTexto.setAttribute("type", "text")
-        caixaDeTexto.setAttribute("id", `nova_categoria_${dado.Localizador}`)
-        caixaDeTexto.setAttribute("value", dado.Categoria)
+        let caixaDeTexto = document.createElement("input"); // Declarando a variável caixaDeTexto como sendo um input
+        caixaDeTexto.setAttribute("type", "text");// declarando o tipo do input que nesse caso é de texto
+        caixaDeTexto.setAttribute("id", `nova_categoria_${dado.Localizador}`);// Setando o atributo id no input e passando que o id vai ser nova-categoria_{numero do localizador}
+        caixaDeTexto.setAttribute("class", "input-style");// colocando uma classe no input para estiliza-lo
+        caixaDeTexto.setAttribute("value", dado.Categoria);// passando que o valor default vai ser oque estiver na coluna categoria do banco de dados, daquele item.
+        caixaDeTexto.disabled = true; // passando que o input vai estar desativado.
 
-        let buttonCategoria = document.createElement("button");
-        buttonCategoria.setAttribute("id", `atualizar-categoria-btn_${dado.Localizador}` )
-        buttonCategoria.setAttribute('onclick', `atualizarCategorias(${dado.Localizador})`);
-        buttonCategoria.innerText = "Atualizar Categoria"
+        
+        let imgEdit = document.createElement('img');// declarando uma variável como sendo um elemento img
+        imgEdit.src = 'img/lapis.png'// passando que esse elemento img vai ter como source a pasta img e o arquivo lapis.png
+        
+        let buttonEdit = document.createElement("button");// declarando a variável buttonEdit como sendo um elemento button
+        buttonEdit.setAttribute("class", "ativar-input");// colocando uma classe no botao para estiliza-lo
+        buttonEdit.appendChild(imgEdit);// colocando a imgEdit dentro no botão
+        buttonEdit.addEventListener("click", function () {// adicionando um evento de click
+            caixaDeTexto.disabled = false;// quando o botao for clicado vai ativar o input
+            buttonEdit.style.display = "none";// o buttonEdit vai sumir
+            buttonCategoria.style.display = "inline";// e o buttonCategoria vai aparecer com o display inline
+            caixaDeTexto.focus();// colocando o foco no input quando o botão for clicado
+        });
 
-        tdCategoria.appendChild(caixaDeTexto)
+        let imgDone = document.createElement('img');// declarando uma variável como sendo um elemento img
+        imgDone.src = 'img/verificar.png';// passando que esse elemento img vai ter como source a pasta img e o  arquivo verificar.png
 
-        tdCategoria.appendChild(buttonCategoria);
+        
+        let buttonCategoria = document.createElement("button");// declarando a variável buttonCategoria como sendo um elemento button
+        buttonCategoria.setAttribute("id", `atualizar-categoria-btn_${dado.Localizador}`);// setando o atributo id e passando que ele vai ser atualizar-categoria-btn_{numero do localizador} 
+        buttonCategoria.setAttribute("class", "att-category");// colocando uma classe no botão para estiliza-lo
+        buttonCategoria.setAttribute("onclick", `atualizarCategorias(${dado.Localizador})`);// setando o atributo onclick e passando que ele vai chamar a funçao atualizarCategoria e como parametro vai passar o numero do localizar
+        buttonCategoria.style.display = "none"; // Passando que ele vai iniciar oculto
+        buttonCategoria.appendChild(imgDone);// colocando a imgDone no botão
+        buttonCategoria.addEventListener("click", function () {// adicionando um evento de click
+            caixaDeTexto.disabled = true;// quando o botão for clicado o input vai ser desativado
+            buttonEdit.style.display = "inline";// o buttonEdit vai voltar a aparecer com o display inline
+            buttonCategoria.style.display = "none";// o buttonCategoria vai sumir
+            alert("Categoria atualizada com sucesso!")// adicionando um alerta quando a categoria for atualizada com sucesso
+        });
+
+
+        tdCategoria.appendChild(caixaDeTexto);// populando a coluna tdCategoria com o input
+        tdCategoria.appendChild(buttonEdit);// populando a coluna tdCategoria com o buttonEdit
+        tdCategoria.appendChild(buttonCategoria);// populando a coluna categoria com o buttonCategoria
 
         tr.appendChild(tdCategoria); // appendendo o tdCategoria à linha tr
 
@@ -109,9 +138,11 @@ async function atualizarCategorias(id) {
     let jsonData;
 
     try {
-        jsonData = await atualizeCategories(id, categoria)
+        jsonData = await atualizeCategories(id, categoria);
+        // document.querySelector(`#nova_categoria_${id}`).value = categoria;
+        // alert("Categoria atualizada com sucesso!");
     } catch (error) {
-        alert("Não foi possivel.")
+        alert("Não foi possivel atualizar a categoria.")
         location.reload();
         return;
     }
